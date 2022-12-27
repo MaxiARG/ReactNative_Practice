1421cd782fb39325e76d6f6c4fa0f21efec46d79
MaxiARG <maxiramos2017@gmail.com>
2022-12-27 19:47:00 -0300

    config RNE RNN

--- PracticeApp/App.tsx
+++ PracticeApp/App.tsx
@@ -1,8 +1,33 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable semi */
 /* eslint-disable react/self-closing-comp */
+import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {NavigationContainer} from '@react-navigation/native';
+import {Text, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
-const App = () => {
-  return <SafeAreaProvider></SafeAreaProvider>;
-};
+const Drawer = createDrawerNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 export default App;
